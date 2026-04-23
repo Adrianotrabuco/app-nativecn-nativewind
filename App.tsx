1405a6b10e8bfbf0974c9106354c61244c668bbe
Adriano Trabuco <adriano.trabuco@senacsp.edu.br>
2026-04-23 20:22:11 -0300

    fix: corrige App.tsx

--- App.tsx
+++ App.tsx
@@ -1,28 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import "./global.css";
 import { Profile } from '@/app/Profile';
-<<<<<<< HEAD
 import { ToastProvider } from '@/components/Toast';
-=======
->>>>>>> a72115172c87ccf3be6918a2ec1c00d8b6e8262b
 
 export default function App() {
   return (
-    <>
-<<<<<<< HEAD
-      <ToastProvider>
-        <Profile />
-        <StatusBar style="light" />
-      </ToastProvider>
-    </>
-  );
-}
-=======
+    <ToastProvider>
       <Profile />
       <StatusBar style="light" />
-    </>
+    </ToastProvider>
   );
 }
-
-
->>>>>>> a72115172c87ccf3be6918a2ec1c00d8b6e8262b
